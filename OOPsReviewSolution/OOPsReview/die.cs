aeff4df8d8fae1e83c8d5bb95e860d17d9374cf9
jbarnsley2 <46543289+jbarnsley2@users.noreply.github.com>
2019-01-11 16:14:34 -0700

    Create Colour

--- OOPsReviewSolution/OOPsReview/die.cs
+++ OOPsReviewSolution/OOPsReview/die.cs
@@ -19,6 +19,8 @@ namespace OOPsReview
         // public data members CAN be reached directly by the user
 
         private int _Size;
+        private string _Colour;
+
 
         //Properties
         // a property is an external infterface between the user and a single
@@ -38,12 +40,60 @@ namespace OOPsReview
             }
             set
             {
-                // takes the supplied user value and places it into the private data member
+                // takes the supplied user value and places it into the internal private data member
                 // the incoming piece of data is placed into a special variable that is called: value
-                _Size = value;
+                // optionally you may place validation on the incoming value
+                if (value >= 6 && value <= 20)
+                {
+                    _Size = value;
+
+                }
+                else
+                {
+                    throw new Exception("Die cannot be" + value.ToString() + "sides. Die must have between 6 and 20 sides.");
+
+                }
+
 
             }
         }
+        // Auto Implemented Property
+        //public
+        // It has a datatype
+        // It has a name
+        // IT DOES NOT have an internal data member that you can DIRECTLY access
+        // The system will create, internally, a data storage area of the appropriate datatype and manage the area
+        // The only way to access the data of an Auto Implemented Property is via the property
+        // usually use when there is no need for any internal validation or other property logic
+
+        public int FaceValue { get; set; }
+
+        public string Colour
+        {
+
+            get
+            {
+                return _Colour;
+            }
+            set
+            {
+
+                //(value == null) this will fail for an empty string
+                // (value == "") this will fail for a null value 
+                // never enough validation. 
+                if (string.IsNullOrEmpty(value))   // isnullorempty expects a string value
+                {
+                    throw new Exception("Colour has no value.");
+                }
+                else
+                {
+                    _Colour = value;
+                 
+                }
+            }
+
+        }
+
 
         //Constructors
 
