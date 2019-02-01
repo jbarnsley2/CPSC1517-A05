a71f5b06f0e64712f51115fd0bfa840a6bca62db
jbarnsley2 <46543289+jbarnsley2@users.noreply.github.com>
2019-02-01 10:03:03 -0700

    Add files via upload

---
+++ cpsc1517A05-master/OOPsReviewSolution/OOPsReview/Turn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Turn
+    {
+        private int _Player1Roll;
+
+        public int Player1Roll
+        {
+            get
+            {
+                return _Player1Roll;
+            }
+            set
+            {
+                _Player1Roll = value;
+            }
+        }
+        public int Player2Roll { get; set; }
+
+        public Turn()
+        {
+
+        }
+
+        public Turn(int player1, int player2)
+        {
+            Player1Roll = player1;
+            Player2Roll = player2;
+        }
+
+        //methods
+        //public string FindRollResults()
+        //{
+        //     return null;
+        //}
+    }
+}
