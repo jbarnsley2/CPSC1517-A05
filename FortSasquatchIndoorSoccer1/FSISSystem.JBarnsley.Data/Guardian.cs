e488c68d6bb70cd58e5666be70c045ca7165b98f
jbarnsley2 <46543289+jbarnsley2@users.noreply.github.com>
2019-03-20 16:25:24 -0600

    Exercise05&06 Updated

---
+++ FortSasquatchIndoorSoccer1/FSISSystem.JBarnsley.Data/Guardian.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namspace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace FSISSystem.JBarnsley.Data
+{
+  
+        [Table("Guardian", Schema = "dbo")]
+        public class Guardian
+        {
+            [Key]
+            public int GuardianID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            private string _EmergencyPhoneNumber;
+            private string _EmailAddress;
+            [NotMapped]
+            public string FullName { get { return LastName + "," + FirstName; } }
+
+            public string EmergencyPhoneNumber
+            {
+                get
+                {
+                    return _EmergencyPhoneNumber;
+
+                }
+                set
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        _EmergencyPhoneNumber = null;
+                    }
+                    else
+                    {
+                        _EmergencyPhoneNumber = value;
+                    }
+                }
+            }
+
+            public string EmailAddress
+            {
+                get
+                {
+                    return _EmailAddress;
+                }
+                set
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        _EmailAddress = null;
+                    }
+                    else
+                    {
+                        _EmailAddress = value;
+                    }
+                }
+            }
+
+
+        }
+    }
+
